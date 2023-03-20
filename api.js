335474855a74de262725ca11f970b0a740e37a74
Ben Hall <ben@benhall.me.uk>
2023-03-20 10:51:06 +0000

    Decode user details

--- api.js
+++ api.js
@@ -3,11 +3,18 @@ require('dotenv').config()
 const express = require('express');
 const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 
+const jwt = require('jsonwebtoken');
+
+
 const app = express();
 // https://dev-shoulder.eu.auth0.com/.well-known/openid-configuration
 app.use(auth({ secret: false }));
 
 app.get('/products', requiredScopes('read:products'), (req, res) => {
+  const auth = req.auth;
+  console.log(auth, jwt.decode(auth.token));
+  const user = jwt.decode(auth.token);
+  console.log(user.sub);
   res.json([
     { id: 1, name: 'Football boots' },
     { id: 2, name: 'Running shoes' },
